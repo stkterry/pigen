a242d735253813461f87e50e5db85305477b4e48
Steven K. Terry <stkterry@gmail.com>
2020-02-08 08:57:40 -0500

    Push before bump

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ setup(
     install_requires=requirements,
     license="MIT license",
     long_description_content_type='text/x-rst',
-    long_description=f'{readme}\n\n{history}',
+    long_description=readme + '\n\n' + history,
     include_package_data=True,
     keywords='pigen',
     name='pigen',
