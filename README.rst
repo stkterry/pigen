92f829ddd96a950b5b4763328540118def01f2ee
Steven K. Terry <stkterry@gmail.com>
2020-02-08 02:35:34 -0500

    initial commit, docs incomplete, readme incomplete

---
+++ README.rst
@@ -0,0 +1,48 @@
+==================================================
+CollatzPy : Collatz Visualization and Exploration
+==================================================
+
+
+.. image:: https://img.shields.io/pypi/v/pigen.svg
+        :target: https://pypi.python.org/pypi/pigen
+
+.. image:: https://img.shields.io/travis/stkterry/pigen.svg
+        :target: https://travis-ci.org/stkterry/pigen
+
+.. image:: https://readthedocs.org/projects/pigen/badge/?version=latest
+        :target: https://pigen.readthedocs.io/en/latest/?badge=latest
+        :alt: Documentation Status
+
+
+Overview
+########
+
+CollatzPy is a package that lets users create tree objects that collect
+Collatz sequences as defined by the popular Collatz conjecture.  The
+package contains a small set of functions for saving and recalling trees as
+wel as visualization tools and tree graphing.  
+
+The package is, by and large, a simple one; more robust and powerful tools exist
+for this task. But this one at the very least is simple, relatively efficient 
+for storing a few million sequences, and the trees themselves are fairly
+compact.  Plotting tools use matplotlib/pygraphviz and are preconfigured for 
+quick and decent looking visualization.
+
+
+Components
+##########
+
+
+
+
+
+
+* Free software: MIT license
+
+Features
+--------
+
+* TODO
+
+Credits
+-------
---
+++ setup.py
@@ -0,0 +1,51 @@
+#!/usr/bin/env python
+
+"""setup"""
+
+from setuptools import setup, find_packages
+
+with open('README.rst') as readme_file:
+    readme = readme_file.read()
+
+with open('HISTORY.rst') as history_file:
+    history = history_file.read()
+
+requirements = []
+
+setup_requirements = []
+
+test_requirements = []
+
+setup(
+    author="Steven K Terry",
+    author_email='stkterry@gmail.com',
+    python_requires='>=3.5',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+    ],
+    description="A collection of Pi generators.",
+    entry_points={},
+    install_requires=requirements,
+    license="MIT license",
+    long_description=readme + '\n\n' + history,
+    include_package_data=True,
+    keywords='pigen',
+    name='pigen',
+    packages=find_packages(include=['pigen', 'pigen.*']),
+    setup_requires=setup_requirements,
+    test_suite='tests',
+    tests_require=test_requirements,
+    url='https://github.com/stkterry/pigen',
+    version='0.1.0',
+    zip_safe=False,
+    data_files=[
+
+    ]
+)
